b102f3724075085883c9dbaccd19c1d2d1617ee8
Anna Ślimak <slimakania@gmail.com>
2019-06-06 10:51:38 +0200

    Added Mapbox

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
+import mapboxgl from "mapbox-gl/dist/mapbox-gl";
 
 function App() {
-  return <div id="widget">Hello widget</div>;
+  const loadMap = () => {
+    mapboxgl.accessToken =
+      "pk.eyJ1IjoiYW5uYS1zbGltYWsiLCJhIjoiY2p2bWN4Ym5hMTdzcDQ5bWwxdTFocnlrdSJ9._dCxzb46y5Yvs9xDsfbQyg";
+    var map = new mapboxgl.Map({
+      container: "map",
+      style: "mapbox://styles/mapbox/streets-v11"
+    });
+  };
+
+  useEffect(loadMap, []);
+
+  return <div id="map" />;
 }
 
 export default App;
