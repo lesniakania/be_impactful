83a84927ba4be7f015b89a4e31dc121cbc679197
Anna Ślimak <slimakania@gmail.com>
2019-06-14 09:43:05 +0200

    Display actual alert properties

--- src/Alert.js
+++ src/Alert.js
@@ -1,8 +1,15 @@
 export default function Alert(feature) {
   const properties = feature.properties;
   return {
-    name: properties.Tipo,
-    description: properties.Nar,
-    coordinates: feature.geometry.coordinates[0][0]
+    key: "alert-" + properties.title.replace(" ", "_"),
+    coordinates: feature.geometry.coordinates[0][0],
+    title: properties.title,
+    category: properties.category,
+    community: properties.community,
+    description: properties.description,
+    action: properties.action,
+    photos: JSON.parse(properties.photos),
+    alertDate: properties.alert_date,
+    fieldDate: properties.field_date
   };
 }
--- src/Popup/Popup.js
+++ src/Popup/Popup.js
@@ -5,11 +5,47 @@ import { showPopup } from "../MapBoxApi";
 import "./Popup.css";
 
 class Popup extends React.Component {
+  dates() {
+    let datesArray = [];
+    if (this.props.fieldDate) {
+      datesArray.push(`Field date: ${this.props.fieldDate}`);
+    }
+    if (this.props.alertDate) {
+      datesArray.push(`Alert date: ${this.props.alertDate}`);
+    }
+
+    return <h6>{datesArray.join(", ")}</h6>;
+  }
+
+  action() {
+    if (this.props.action) {
+      return <div>{this.props.action}</div>;
+    }
+  }
+
+  photos() {
+    const ul = this.props.photos.map((photoUrl, index) => {
+      return (
+        <li key={`photo-${index}`}>
+          <img src={photoUrl} alt="photo" />
+        </li>
+      );
+    });
+    return <ul>{ul}</ul>;
+  }
+
   render() {
     return (
       <div>
-        <h3>{this.props.name}</h3>
+        <h3>{this.props.title}</h3>
+        <h5>
+          {this.props.category}, {this.props.community}
+        </h5>
+        {this.dates()}
         {this.props.description}
+
+        {this.action()}
+        {this.photos()}
       </div>
     );
   }
@@ -21,8 +57,14 @@ class Popup extends React.Component {
 }
 
 Popup.propTypes = {
-  name: PropTypes.string,
-  description: PropTypes.string
+  title: PropTypes.string,
+  category: PropTypes.string,
+  community: PropTypes.string,
+  description: PropTypes.string,
+  action: PropTypes.string,
+  photos: PropTypes.array,
+  alertDate: PropTypes.string,
+  fieldDate: PropTypes.string
 };
 
 export default Popup;
