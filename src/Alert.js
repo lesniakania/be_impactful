52a6f24b4f102fdebd3863095194a4c2685fbf6f
Anna Ślimak <slimakania@gmail.com>
2019-06-17 10:41:27 +0200

    Support all types of geometries

--- src/Alert.js
+++ src/Alert.js
@@ -2,14 +2,37 @@ export default function Alert(feature) {
   const properties = feature.properties;
   return {
     key: "alert-" + properties.title.replace(" ", "_"),
-    coordinates: feature.geometry.coordinates,
+    coordinates: coordinates(feature),
     title: properties.title,
     category: properties.category,
     community: properties.community,
     description: properties.description,
     action: properties.action,
-    photos: JSON.parse(properties.photos),
+    photos: photos(properties),
     alertDate: properties.alert_date,
     fieldDate: properties.field_date
   };
+}
+
+function photos(properties) {
+  if (properties.photos) {
+    return JSON.parse(properties.photos);
+  } else {
+    return [];
+  }
+}
+
+function coordinates(feature) {
+  const geometry = feature.geometry;
+  const coordinates = geometry.coordinates;
+
+  if (geometry.type === "Point") {
+    return coordinates;
+  } else if (geometry.type === "Polygon") {
+    return coordinates[0][0];
+  } else if (geometry.type === "LineString") {
+    return coordinates[0];
+  } else {
+    throw "Unsupported geometry";
+  }
 }
