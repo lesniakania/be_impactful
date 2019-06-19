77c4beda7a3e0ff4ae401c0126be4f33be319508
Anna Ślimak <slimakania@gmail.com>
2019-06-19 10:32:36 +0200

    Removed the legend for now

--- src/App/App.js
+++ src/App/App.js
@@ -2,7 +2,6 @@ import React, { useEffect } from "react";
 import "./App.css";
 import Sidebar from "../Sidebar/Sidebar";
 import Map from "../Map/Map";
-import Legend from "../Legend/Legend";
 import GlobalStoreContext, { useGlobalReducer } from "../GlobalStore";
 import {
   loadMap as loadMapAction,
@@ -26,7 +25,6 @@ function App() {
       <div className="App">
         <Sidebar map={state.app.map} onPopupShow={showPopup} />
         <Map map={state.app.map} onPopupShow={showPopup} />
-        <Legend />
       </div>
     </GlobalStoreContext.Provider>
   );
